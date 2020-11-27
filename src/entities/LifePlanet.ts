0099c56e8c0957244d105689db6bcab099a0d251
Eduardo Pinho <enet4mikeenet@gmail.com>
2020-11-27 21:48:06 +0000

    Increase default life planet mass a bit

--- src/entities/LifePlanet.ts
+++ src/entities/LifePlanet.ts
@@ -6,7 +6,7 @@ export interface LifePlanet extends SpaceThing {
     power: number;
 }
 
-export function create(id: string, x: number, y: number, size: number = DEFAULT_LIFE_PLANET_SIZE, mass: number = 16, textureId: string = 'planet_blue', power: number = 2): LifePlanet {
+export function create(id: string, x: number, y: number, size: number = DEFAULT_LIFE_PLANET_SIZE, mass: number = 20, textureId: string = 'planet_blue', power: number = 2): LifePlanet {
     return {
         id, x, y, size, mass, textureId, power
     };
