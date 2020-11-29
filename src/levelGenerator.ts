ef7b97b0fb60030a3c970083241b6e08a7c411fb
Eduardo Pinho <enet4mikeenet@gmail.com>
2020-11-29 12:43:36 +0000

    More randomness in level generator

--- src/levelGenerator.ts
+++ src/levelGenerator.ts
@@ -19,11 +19,14 @@ export function generateLevel(size: number, numPlanets: number, seed?: any): Lev
     let rng = seedrandom(seed, {entropy: true});
     random.use(rng);
 
+    numPlanets += random.uniformInt(-1, 1)();
+    size += random.uniformInt(-150, 150)();
+
     // maximum power based on level size
     const power = size >= 2000 ? 5 : size > 1600 ? 4 : 3;
 
     let wSize = size;
-    let hSize = size * 0.8;
+    let hSize = Math.round(size * 0.75);
     let bounds = {
         left: -wSize,
         top: -hSize,
@@ -78,7 +81,7 @@ export function generateLevel(size: number, numPlanets: number, seed?: any): Lev
         let attemptsLeft = 10;
         while (attemptsLeft > 0) {
             let r = 40 + thingRadius() * 100;
-            let mass = thingMassFactor() * r * r * 0.1;
+            let mass = thingMassFactor() * r * r * 0.08;
             
             if (thingAntiMatter() === 1) {
                 // anti-matter thing, repels instead of attracting
