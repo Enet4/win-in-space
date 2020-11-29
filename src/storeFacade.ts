169d790437a9618b0dd43522d5ab26bcee09fce6
Eduardo Pinho <enet4mikeenet@gmail.com>
2020-11-29 13:49:33 +0000

    Fix storeFacade

--- src/storeFacade.ts
+++ src/storeFacade.ts
@@ -1,16 +1,24 @@
 // since some environments such as a cross-site iframe 
 // might not have localStorage, this provides a non-persistent workaround
-export default (typeof window.localStorage === 'object') ? window.localStorage : {
+const storage = {
     musicEnabled: null,
     getItem(key: string) {
+        if (localStorage && localStorage.getItem) {
+            return window.localStorage.getItem(key);
+        }
         if (key === 'musicEnabled') {
             return this.musicEnabled;
         }
         return null;
     },
     setItem(key: string, value: string) {
+        if (localStorage && localStorage.setItem) {
+            return window.localStorage.setItem(key, value);
+        }
         if (key === 'musicEnabled') {
             this.musicEnabled = value;
         }
     }
 };
+
+export default storage;
